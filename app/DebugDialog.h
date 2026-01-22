3a93cfb5206301b59fa79e452e652e25f7f7970c
Arnaud <arnaud@status.im>
2026-01-22 18:49:28 +0400

    Remove unused files

--- app/DebugDialog.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <QDialog>
-
-class DebugDialog : public QDialog {
-    Q_OBJECT
-  public:
-    explicit DebugDialog(QWidget* parent = nullptr, const QString& debug = "");
-};
