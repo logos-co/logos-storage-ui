e658758888115fdeb3559d45ba082708677d0636
Arnaud <arnaud@status.im>
2026-08-14 16:28:47 +0400

    Update Storage Module

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder";
     nix-bundle-lgx.url = "github:logos-co/nix-bundle-lgx";
-    storage_module.url = "github:logos-co/logos-storage-module?ref=refs/tags/v2.1.1";
+    storage_module.url = "github:logos-co/logos-storage-module?ref=refs/tags/v2.1.2";
   };
 
   outputs = inputs@{ logos-module-builder, ... }:
